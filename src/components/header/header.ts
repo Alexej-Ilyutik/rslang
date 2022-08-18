9f7eb1263e659564a77dc180e91d47a02c8df904
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-08-18 21:49:43 +0300

    feat: add header and footer

---
+++ src/components/header/header.ts
@@ -0,0 +1,65 @@
+import './header.scss';
+
+export const renderHeader = (): void => {
+  const headerBlock = `
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="./assets/united-kingdom.svg" alt="logo" width="75" height="65" />
+            <div>RS Lang</div>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 navbar-links">
+              <li class="nav-item mx-3">
+                <a
+                  class="nav-link fw-bolder fs-5 active"
+                  aria-current="page"
+                  href="#/" onclick="onNavigate('/'); return false;"
+                  >Главная</a
+                >
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link fw-bolder fs-5" href="#/book"
+                onclick="onNavigate('/book'); return false;"
+                  >Учебник</a
+                >
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link fw-bolder fs-5" href="#/games"
+                onclick="onNavigate('/games'); return false;">Игры</a>
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link fw-bolder fs-5" href="#/statistic"
+                onclick="onNavigate('/statistic'); return false;">Статистика</a>
+              </li>
+              <li class="nav-item mx-3">
+                <a class="nav-link fw-bolder fs-5" href="#/command"
+                onclick="onNavigate('/command'); return false;">Команда</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-lg-0 navbar-btn-group">
+              <li class="nav-item">
+                <button type="button" class="btn btn-primary">Log in</button>
+              </li>
+               <li class="nav-item">
+                <button type="button" class="btn btn-secondary">Register</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+`;
+  const header = document.getElementById('header') as HTMLElement;
+  header.innerHTML = headerBlock;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,66 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { renderHeader } from './components/header/header';
+import { renderFooter } from './components/footer/footer';
 import { renderMain } from './components/pages/main/main';
 import { textBook } from './components/pages/textBook/textBook';
 import './style.scss';
 
-// renderPage();
-
 export const renderPage = (): void => {
-  const pageContent = `
-         ${renderMain()}
-`;
-  const app = document.createElement('div');
-  app.innerHTML = pageContent;
-  document.body.appendChild(app);
+  renderHeader();
+  renderFooter();
+  // renderMain();
 };
+
 renderPage();
 
+const main = document.getElementById('main') as HTMLElement;
+
+
+const onNavigate = (location: string): void => {
+  switch (location) {
+    case '/':
+      main.innerHTML = `<h1>Main</h1>`;
+      break;
+    case '/book':
+      main.innerHTML = `<h1>Book</h1>`;
+      break;
+    case '/games':
+      main.innerHTML = `<h1>Games</h1>`;
+      break;
+    case '/statistic':
+      main.innerHTML = `<h1>Statistic</h1>`;
+      break;
+    case '/command':
+      main.innerHTML = `<h1>Command</h1>`;
+      break;
+    default:
+      main.innerHTML = `<h1>Main</h1>`;
+      break;
+  }
+};
+
+window.addEventListener('load', ()=>{
+  const location = window.location.hash;
+  if(location){
+    onNavigate(location);
+  }
+})
+
 // const routes = {
-//   '/': main,
-//   '/textBook': textBook,
+//   '/': home,
+//   '/book': book,
+//   '/games': games,
 // };
 
-// const rootDiv = document.getElementById('root') as HTMLElement;
-// rootDiv.innerHTML = routes[window.location.pathname];
+// const main = document.getElementById('main') as HTMLElement;
+// main.innerHTML = routes[window.location.pathname];
 
-// const onNavigate = pathname => {
+// const onNavigate = (pathname: void) => {
 //   window.history.pushState({}, pathname, window.location.origin + pathname);
-//   rootDiv.innerHTML = routes[pathname];
+//   main.innerHTML = routes[pathname];
 // };
 
-// window.onpopstate = () => {
-//   rootDiv.innerHTML = routes[window.location.pathname];
+// window.onpopstate = (): void => {
+//   main.innerHTML = routes[window.location.pathname];
 // };
