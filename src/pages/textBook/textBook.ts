15c0ce01b6409f7389677fdd6ad29eff98e69344
NewAnton <1anton1korobov@gmail.com>
2022-08-21 11:49:54 +0300

    feat: add styles to text book page

--- src/pages/textBook/textBook.ts
+++ src/pages/textBook/textBook.ts
@@ -8,21 +8,24 @@ export const renderTextBook = (): void => {
       <h2 class="textBook__title">Text book</h2>
       <div class="textBook__content-wrapper">
         <nav class="textBook__nav nav nav-pills flex-column flex-sm-row">
-          <a class="flex-sm-fill text-sm-center nav-link active" href="#">Active</a>
-          <a class="flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
-          <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-          <a class="flex-sm-fill text-sm-center nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="flex-sm-fill text-sm-center nav-link active" href="#none">A1</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#none">A2</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#none">B1</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#none">B2</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#none">C1</a>
+          <a class="flex-sm-fill text-sm-center nav-link" href="#none">C2</a>
+          <a class="flex-sm-fill text-sm-center nav-link disabled" href="#none" tabindex="-1" aria-disabled="true">H</a>
         </nav>
-        <nav aria-label="Page navigation example">
-          <ul class="pagination justify-content-center">
+        <nav class="textBook__pagination" aria-label="Page navigation example">
+          <ul class="textBook__pagination_list pagination justify-content-center">
             <li class="page-item disabled">
-              <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+              <a class="page-link" href="#" tabindex="-1" aria-disabled="true">\<</a>
             </li>
             <li class="page-item"><a class="page-link" href="#">1</a></li>
             <li class="page-item"><a class="page-link" href="#">2</a></li>
             <li class="page-item"><a class="page-link" href="#">3</a></li>
             <li class="page-item">
-              <a class="page-link" href="#">Next</a>
+              <a class="page-link" href="#">\></a>
             </li>
           </ul>
         </nav>
@@ -40,7 +43,7 @@ export const renderTextBook = (): void => {
       "group": 1,
       "page": 1,
       "word": "duck",
-      "image": "src\assets\duck.png",
+      "image": "../../assets/duck.png",
       "audio": "string",
       "audioMeaning": "string",
       "audioExample": "string",
@@ -56,7 +59,7 @@ export const renderTextBook = (): void => {
       "group": 1,
       "page": 1,
       "word": "duck2",
-      "image": "src\assets\duck.png",
+      "image": "../../assets/duck.png",
       "audio": "string",
       "audioMeaning": "string",
       "audioExample": "string",
@@ -77,7 +80,7 @@ export const renderTextBook = (): void => {
     <li class="textBook__words-list_word-card word-card">
       <img class="word-card_img" src="${element.image}" alt="${element.word} image"></img>
       <div class="word-card_information-wrapper">
-        <p class="word-card_word-name">${element.word}</p>
+        <h2 class="word-card_word-name">${element.word}</h2>
         <p class="word-card_word-transcription">${element.transcription}</p>
         <p class="word-card_word-name-translation">${element.wordTranslate}</p>
         <p class="word-card_word-meaning">${element.textMeaning}</p>
