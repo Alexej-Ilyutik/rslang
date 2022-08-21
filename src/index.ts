7db77f17bb6e17f0515300b2428e86b1a6e836ce
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-08-21 12:59:43 +0300

    feat: add footer

--- src/index.ts
+++ src/index.ts
@@ -46,22 +46,31 @@ window.addEventListener('click', (e: Event) => {
   const target = e.target as HTMLAnchorElement;
 
   if (target.classList.contains('nav-link')) {
-
     deleteClassActive(navLinks);
 
     const location = target.href.split('/').slice(-2).join('/');
-    target.classList.add('active');
+    navLinks.forEach(el => {
+      if (el.innerHTML === target.innerHTML) {
+        el.classList.add('active');
+      }
+    });
+
     onNavigate(location);
   }
 });
 
 const brand = document.querySelector('.navbar-brand') as HTMLElement;
+const footerBrand = document.querySelector('.footer__brand') as HTMLElement;
 
-brand.addEventListener('click', (e: Event) => {
+function changeLinkActiveLogo(e: Event) {
   deleteClassActive(navLinks);
   const curTarget = e.currentTarget as HTMLAnchorElement;
   const location = curTarget.href.split('/').slice(-2).join('/');
 
   mainLink.classList.add('active');
   onNavigate(location);
-});
+}
+
+brand.addEventListener('click', changeLinkActiveLogo);
+
+footerBrand.addEventListener('click', changeLinkActiveLogo);
