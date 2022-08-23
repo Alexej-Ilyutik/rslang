5f90d07fc5f291de1491ae5e12bf8443937926a7
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-08-23 13:38:24 +0300

    feat: add section audio

--- src/pages/games/game.ts
+++ src/pages/games/game.ts
@@ -17,69 +17,38 @@ export const renderGamePage = (): void => {
 			</div>
 		</article>
 		<article class="postcard light red">
-			<a class="postcard__img_link" href="#">
-				<img class="postcard__img" src="https://picsum.photos/501/500" alt="Image Title" />
+			<a class="postcard__img_link" href="#/audio">
+				<img class="postcard__img" src="../../assets/audio-img.png" alt="audio" alt="Image Title" />
 			</a>
 			<div class="postcard__text t-dark">
-				<h2 class="postcard__title red"><a href="#">Podcast Title</a></h2>
-				<div class="postcard__subtitle small">
-					<time datetime="2020-05-25 12:00:00">
-						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-					</time>
-				</div>
+				<h2 class="postcard__title red"><a href="#/audio">Audio challenge</a></h2>
 				<div class="postcard__bar"></div>
-				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-				<ul class="postcard__tagbox">
-					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-					<li class="tag__item play red">
-						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-					</li>
-				</ul>
+				<div class="postcard__preview-txt">Check your listening skills,
+        trying to pick the right meaning after hearing a word. Be careful, as you just have one guess.</div>
+				<a href="#/audio" class="game__btn btn btn-warning" role="button">Play game</a>
 			</div>
 		</article>
 		<article class="postcard light green">
 			<a class="postcard__img_link" href="#">
-				<img class="postcard__img" src="https://picsum.photos/500/501" alt="Image Title" />
+				<img class="postcard__img" src="../../assets/london.jpg" alt="Image Title" />
 			</a>
 			<div class="postcard__text t-dark">
-				<h2 class="postcard__title green"><a href="#">Podcast Title</a></h2>
-				<div class="postcard__subtitle small">
-					<time datetime="2020-05-25 12:00:00">
-						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-					</time>
-				</div>
+				<h2 class="postcard__title green"><a href="#">Other games</a></h2>
 				<div class="postcard__bar"></div>
 				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-				<ul class="postcard__tagbox">
-					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-					<li class="tag__item play green">
-						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-					</li>
-				</ul>
+				<a href="#/audio" class="game__btn btn btn-warning" role="button">Play game</a>
 			</div>
 		</article>
 		<article class="postcard light yellow">
 			<a class="postcard__img_link" href="#">
-				<img class="postcard__img" src="https://picsum.photos/501/501" alt="Image Title" />
+				<img class="postcard__img" src="../../assets/london.jpg" alt="Image Title" />
 			</a>
 			<div class="postcard__text t-dark">
-				<h2 class="postcard__title yellow"><a href="#">Podcast Title</a></h2>
-				<div class="postcard__subtitle small">
-					<time datetime="2020-05-25 12:00:00">
-						<i class="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-					</time>
-				</div>
+				<h2 class="postcard__title yellow"><a href="#">Other games</a></h2>
+
 				<div class="postcard__bar"></div>
 				<div class="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-				<ul class="postcard__tagbox">
-					<li class="tag__item"><i class="fas fa-tag mr-2"></i>Podcast</li>
-					<li class="tag__item"><i class="fas fa-clock mr-2"></i>55 mins.</li>
-					<li class="tag__item play yellow">
-						<a href="#"><i class="fas fa-play mr-2"></i>Play Episode</a>
-					</li>
-				</ul>
+			<a href="#/audio" class="game__btn btn btn-warning" role="button">Play game</a>
 			</div>
 		</article>
 	</div>
