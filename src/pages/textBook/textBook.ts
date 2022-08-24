4d8767e0d159acea10664359d8cbc2cafac5108a
NewAnton <1anton1korobov@gmail.com>
2022-08-24 09:50:28 +0300

    fix: checkboxes can switch

--- src/pages/textBook/textBook.ts
+++ src/pages/textBook/textBook.ts
@@ -55,10 +55,16 @@ export const renderTextBoxPage = async (groupNumber: number, pageNumber: number)
           <p class="word-card_word-example-translation">${element.textExampleTranslate}</p>
           <hr class="word-card_line">
           <div class="word-card_status-wrapper">
-            <input type="checkbox" class="word-card_status-checkbox" name="hard" value="yes" checked="checked">
-            <label for="hard">Hard word</label>
-            <input type="checkbox" class="word-card_status-checkbox" name="learned" value="yes">
-            <label for="learned">Learned</label>
+            <div class="word-card_status-checkbox-wrapper">
+              <input type="checkbox" class="word-card_status-checkbox" id="hard" value="yes" checked="checked">
+              <label for="hard"></label>
+              <p class="word-card_status-checkbox-text">Hard word</p>
+            </div>
+            <div class="word-card_status-checkbox-wrapper">
+              <input type="checkbox" class="word-card_status-checkbox" id="learned" value="yes">
+              <label for="learned"></label>
+              <p class="word-card_status-checkbox-text">Learned word</p>
+            </div>
             <p class="word-card_counter-information">Guessed ${0} times</p>
           </div>
         </div>
