b4e22e69a0d3192e200db32d209cd169ad207ddf
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-08-25 22:28:06 +0300

    feat: add options word

--- src/components/gamePageContainer/gamePageContainer.ts
+++ src/components/gamePageContainer/gamePageContainer.ts
@@ -7,22 +7,22 @@ export const renderGamePageContainer = (): void => {
         <aside class="audiocall__settings settings col-3">
           <h4 class="audiocall__title mb-4">Select the Level:</h4>
           <div class="settings__btns mb-5">
-            <input type="radio" class="btn-check" name="options" id="A1" autocomplete="off">
+            <input type="radio" class="btn-check setting__level" name="level" data-level="0" id="A1" autocomplete="off">
             <label class="btn btn-info" for="A1">A1</label>
 
-            <input type="radio" class="btn-check" name="options" id="A2" autocomplete="off">
+            <input type="radio" class="btn-check setting__level" name="level" data-level="1" id="A2" autocomplete="off">
             <label class="btn btn-info" for="A2">A2</label>
 
-            <input type="radio" class="btn-check" name="options" id="B1" autocomplete="off">
+            <input type="radio" class="btn-check setting__level" name="level" data-level="2" id="B1" autocomplete="off">
             <label class="btn btn-info" for="B1">B1</label>
 
-            <input type="radio" class="btn-check" name="options" id="B2" autocomplete="off">
+            <input type="radio" class="btn-check setting__level" name="level" data-level="3" id="B2" autocomplete="off">
             <label class="btn btn-info" for="B2">B2</label>
 
-            <input type="radio" class="btn-check" name="options" id="C1" autocomplete="off">
+            <input type="radio" class="btn-check setting__level" name="level" data-level="4" id="C1" autocomplete="off">
             <label class="btn btn-info" for="C1">C1</label>
 
-            <input type="radio" class="btn-check" name="options" id="C2" autocomplete="off">
+            <input type="radio" class="btn-check setting__level" name="level" data-level="5" id="C2" autocomplete="off">
             <label class="btn btn-info" for="C2">C2</label>
           </div>
           <button type="button" class="btn btn-success settings__start">Start</button>
--- src/pages/audioGame/startAudioGame.ts
+++ src/pages/audioGame/startAudioGame.ts
@@ -1,4 +1,3 @@
-import './audioGame.scss';
 import { WordInterface } from '../../shared/types';
 import API from '../../services/api';
 
@@ -12,37 +11,34 @@ const getWordsTemp = async (diff: number) => {
   return (await Promise.all(result)).flat();
 };
 
-// const arrWords = await getWordsTemp(0);
+const arrWords = await getWordsTemp(0);
 
 function getRandomArbitrary(min: number, max: number) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
-function getArrOptions(array: WordInterface[]) {
+function getArrOptions() {
   const arr: WordInterface[] = [];
   let uniqueArray: WordInterface[] = [];
   while (uniqueArray.length < 4) {
-    const el = array[getRandomArbitrary(0, 599)];
+    const el = arrWords[getRandomArbitrary(0, 599)];
     arr.push(el);
     uniqueArray = [...new Set(arr)];
   }
   return uniqueArray;
 }
 
-// const arrOptions = getArrOptions();
+const arrOptions = getArrOptions();
 
-// const addWordsToAudioGame = (): WordInterface => {
-//   const mainWord = arrOptions[getRandomArbitrary(0, 3)];
-//   return mainWord;
-// };
+const addWordsToAudioGame = (): WordInterface => {
+  const mainWord = arrOptions[getRandomArbitrary(0, 3)];
+  return mainWord;
+};
 
-const renderContentAudioPage = async (
-  block: HTMLElement,
-  mainWord: WordInterface,
-  arrOptions: WordInterface[],
-): Promise<void> => {
-  // const mainWord = addWordsToAudioGame();
+const renderContentAudioPage = async (): Promise<void> => {
+  const audioContent = document.querySelector('.audiocall__content') as HTMLElement;
+  const mainWord = addWordsToAudioGame();
   const mainBlock = `
     <h3 class="audiocall__subtitle">Game: Audio challenge</h3>
     <div class="audiocall__voice">
@@ -66,50 +62,36 @@ const renderContentAudioPage = async (
       <label class="btn btn-warning" for="option4">4. ${`${arrOptions[3].wordTranslate}`}</label>
     </div>
   `;
-  block.innerHTML = mainBlock;
+  audioContent.innerHTML = mainBlock;
 };
 
 let level: number;
 
-const addEventStartAudioGame = (block: HTMLElement): void => {
-  const GameContainer = document.querySelector('.audiocall__container') as HTMLElement;
-  const btnStart = document.querySelector('.settings__start') as HTMLElement;
-  btnStart.classList.add('btn__start-audio-game');
+// const settingsContainer = document.querySelector('.settings__btns') as HTMLElement;
 
-  async function rerenderAudioGame(event: Event) {
+// settingsContainer.addEventListener('click', event => {
+//   const target = event.target as HTMLInputElement;
+//   if (target.classList.contains('setting__level')) {
+//     level = Number(target.getAttribute('data-level'));
+//   }
+// });
+
+export const addEventStartAudioGame = (): void => {
+  const GameContainer = document.querySelector('.audiocall__container') as HTMLElement;
+  // const btnStart = document.querySelector('.settings__start') as HTMLElement;
+  // btnStart.classList.add('btn__start-audio-game');
+
+  GameContainer.addEventListener('click', event => {
     const target = event.target as HTMLInputElement;
 
     if (target.classList.contains('setting__level')) {
       level = Number(target.getAttribute('data-level'));
     }
 
-    if ((event.target as HTMLElement).classList.contains('btn__start-audio-game')) {
+    if (target.classList.contains('btn__start-audio-game')) {
       console.log('работает функция антона');
       console.log(level);
-      const arrWords = await getWordsTemp(level);
-      const arrOptions = getArrOptions(arrWords);
-      const addWordsToAudioGame = (): WordInterface => {
-        const mainWord = arrOptions[getRandomArbitrary(0, 3)];
-        return mainWord;
-      };
-      const mainWord = addWordsToAudioGame();
-      console.log(arrWords);
-
-      renderContentAudioPage(block, mainWord, arrOptions);
+      renderContentAudioPage();
     }
-  }
-
-  GameContainer.addEventListener('click', rerenderAudioGame);
-};
-
-export const renderAudioPage = async (): Promise<void> => {
-  const audioContent = document.querySelector('.audiocall__content') as HTMLElement;
-
-  addEventStartAudioGame(audioContent);
-
-  const mainBlock = `
-    <h3 class="audiocall__subtitle">Game: Audio challenge</h3>
-    <img class="audiocall__img" src="../../assets/audio-img.png" alt="audio" alt="Image Title" />
-  `;
-  audioContent.innerHTML = mainBlock;
+  });
 };
