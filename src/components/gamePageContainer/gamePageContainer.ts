f700ce2bf4bcbad95c93c54aa404fc5b73fe1afe
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-08-25 14:57:56 +0300

    feat: audioGame content

--- src/components/gamePageContainer/gamePageContainer.ts
+++ src/components/gamePageContainer/gamePageContainer.ts
@@ -7,23 +7,23 @@ export const renderGamePageContainer = (): void => {
         <aside class="audiocall__settings settings col-3">
           <h4 class="audiocall__title mb-4">Select the Level:</h4>
           <div class="settings__btns mb-5">
-            <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
-            <label class="btn btn-info" for="option1">A1</label>
+            <input type="radio" class="btn-check" name="options" id="A1" autocomplete="off" checked>
+            <label class="btn btn-info" for="A1">A1</label>
 
-            <input type="radio" class="btn-check" name="options" id="option2" autocomplete="off">
-            <label class="btn btn-info" for="option2">A2</label>
+            <input type="radio" class="btn-check" name="options" id="A2" autocomplete="off">
+            <label class="btn btn-info" for="A2">A2</label>
 
-            <input type="radio" class="btn-check" name="options" id="option3" autocomplete="off">
-            <label class="btn btn-info" for="option3">B1</label>
+            <input type="radio" class="btn-check" name="options" id="B1" autocomplete="off">
+            <label class="btn btn-info" for="B1">B1</label>
 
-            <input type="radio" class="btn-check" name="options" id="option4" autocomplete="off">
-            <label class="btn btn-info" for="option4">B2</label>
+            <input type="radio" class="btn-check" name="options" id="B2" autocomplete="off">
+            <label class="btn btn-info" for="B2">B2</label>
 
-            <input type="radio" class="btn-check" name="options" id="option5" autocomplete="off">
-            <label class="btn btn-info" for="option5">C1</label>
+            <input type="radio" class="btn-check" name="options" id="C1" autocomplete="off">
+            <label class="btn btn-info" for="C1">C1</label>
 
-            <input type="radio" class="btn-check" name="options" id="option6" autocomplete="off">
-            <label class="btn btn-info" for="option6">C2</label>
+            <input type="radio" class="btn-check" name="options" id="C2" autocomplete="off">
+            <label class="btn btn-info" for="C2">C2</label>
           </div>
           <button type="button" class="btn btn-success settings__start">Start</button>
 
--- src/pages/audioGame/audioGame.ts
+++ src/pages/audioGame/audioGame.ts
@@ -3,10 +3,37 @@ import { PageOfWordsInterface } from '../../shared/types';
 import { storage } from '../../shared/storage';
 import API from '../../services/api';
 
-export const renderAudioPage = (): void => {
-  const mainBlock = `
-    <div class="audiocall__voice">
+const getWordsTemp = async (diff: number) => {
+  const result = [];
 
+  for (let i = 0; i <= 29; i += 1) {
+    result.push(API.getWords(diff, i));
+  }
+
+  return (await Promise.all(result)).flat();
+};
+
+const arrWords = await getWordsTemp(0);
+
+function getRandomArbitrary(min:number, max:number) {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+
+
+
+export const renderAudioPage = async (): Promise<void> => {
+  // storage.wordsListCurrentGroup = groupNumber;
+  console.log(arrWords[getRandomArbitrary(0,599)]);
+
+  const mainBlock = `
+    <h3 class="audiocall__subtitle">Game: Audio challenge</h3>
+    <div class="audiocall__voice">
+       <button class="word-card_audio-button">
+          <img class="word-card_audio-button-image" src="../../assets/volume.svg" alt="audio button"
+          data-audio="${`${API.base}/`}"></img>
+        </button>
     </div>
     <div class="audiocall__btns">
       <input type="radio" class="btn-check" name="options" id="option1" autocomplete="off" checked>
